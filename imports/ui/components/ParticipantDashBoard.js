f40aea6db256b43878c52909cfff3b3d13ccc477
Santiago <sanpra1208@gmail.com>
2018-11-23 22:45:56 -0500

    agregado panel de torneos

--- imports/ui/components/ParticipantDashBoard.js
+++ imports/ui/components/ParticipantDashBoard.js
@@ -12,7 +12,7 @@ class ParticipantDashBoard extends Component {
    	 	  <div>
    	 	  	<div className="row">
    	 	  		<div className="col-sm-4 col-md-4 col-lg-4 prueba">
-
+              <TournamentPanel tournaments={[{id:1, name: "Torneo 1"}, {id:2, name: "Torneo 2"}]}/>
             </div>
    	 	  		<div className="col-sm-8 col-md-8 col-lg-8">
    	 	  			<LeaderBoard leaderBoard={[{position:1, name: "Santiago Restrepo", level:"Novato"}, {position:2, name: "Daniel Ocampo", level:"Novato"}]}/>
